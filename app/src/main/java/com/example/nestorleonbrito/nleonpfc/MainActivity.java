ae8877f166e990b6da7cb39554ff5f1a33600dbb
Néstor León <nestor.leon.brito@accenture.com>
2018-05-29 18:22:06 +0200

    Detección de Verdes

--- app/src/main/java/com/example/nestorleonbrito/nleonpfc/MainActivity.java
+++ app/src/main/java/com/example/nestorleonbrito/nleonpfc/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.nestorleonbrito.nleonpfc;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import org.opencv.android.BaseLoaderCallback;
@@ -32,190 +36,94 @@ import static org.opencv.imgproc.Imgproc.getStructuringElement;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    private JavaCameraView cameraView = null;
-    private static boolean initOpenCV = false;
 
-    private Button bt_hacerfoto;
-
-
-
-    Mat mRgba, hsv_scale;
-
-    //callback loader
-    BaseLoaderCallback mCallBackLoader = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-
-
-            switch (status){
-                case BaseLoaderCallback.SUCCESS:
-                    cameraView.enableView();
-                    break;
-                default:
-                    super.onManagerConnected(status);
-                    break;
-            }
+    static {
+        if(!OpenCVLoader.initDebug()){
+            Log.d("TAG", "OenCV no cargado");
         }
-    };
-    public static String TAG="VideoRecordingActivity";
-    public static String TAG2="Video";
+        else{
+            Log.d("TAG", "OpenCV cargado");
+        }
+    }
+    int iLowH = 45;
+    int iHighH = 75;
 
-    static { initOpenCV = OpenCVLoader.initDebug(); }
+    int iLowS = 20;
+    int iHighS = 255;
+
+    int iLowV = 10;
+    int iHighV = 255;
+
+    Mat imgHSV;
+    Mat imgThresholded;
+
+    Scalar sc1;
+    Scalar sc2;
+
+    private JavaCameraView cameraView;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        sc1 = new Scalar(iLowH,iLowS,iLowV);
+        sc2 = new Scalar(iHighH,iHighS,iHighV);
 
         //connect the camera
         cameraView = (JavaCameraView)findViewById(R.id.cameraview);
-
-        //set visibility
-        cameraView.setVisibility(SurfaceView.VISIBLE);
-        cameraView.setMaxFrameSize(1024,768);
-        cameraView.clearFocus();
+        cameraView.getVisibility();
+        cameraView.setCameraIndex(0);
+        cameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
 
         //set callback function
         cameraView.setCvCameraViewListener(this);
 
-        bt_hacerfoto = (Button) this.findViewById(R.id.button1);
-        //Añadimos el Listener Boton
-        bt_hacerfoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                savePicture();
-            }});
+        cameraView.enableView();
     }
 
-    private void savePicture() {
 
-
-    }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        if (OpenCVLoader.initDebug()){
-            Log.d(TAG, "Connected");
-
-            //display when the activity resumed,, callback loader
-            mCallBackLoader.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-
-
-        }else{
-            Log.d(TAG, "Not connected");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0, this, mCallBackLoader);
-        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
-
-        // Release the camera.
-        if (cameraView != null) {
-            cameraView.disableView();
-            cameraView = null;
-        }
+        cameraView.disableView();
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-        //4 channel
-        mRgba = new  Mat(width, height, CvType.CV_8UC4);
-        hsv_scale = new Mat(width, height, CvType.CV_8UC3);
+        imgHSV = new Mat(width,height,CvType.CV_16UC4);
+        imgThresholded = new Mat(width,height,CvType.CV_16UC4);
+
     }
 
     @Override
     public void onCameraViewStopped() {
-        //release
-        mRgba.release();
+
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-//        Mat src = inputFrame.gray(); // convertir a escala de grises
-//        Mat cannyEdges = new Mat();  // objeto para almacenar el resultado
-//
-//        // aplicar el algoritmo canny para detectar los bordes
-//        Imgproc.Canny(src, cannyEdges, 10, 100);
-//
-//        // devolver el objeto Mat procesado
-//        return cannyEdges;
+         Imgproc.cvtColor(inputFrame.rgba(), imgHSV, Imgproc.COLOR_BGR2HSV);
+         Core.inRange(imgHSV,sc1,sc2,imgThresholded);
+        Log.d("TAG", "Por aqui ando");
 
-        //get each frame from camera
-
-        mRgba = inputFrame.rgba();
-
-
-        /**********HSV conversion**************/
-        //convert mat rgb to mat hsv
-        Imgproc.cvtColor(mRgba, hsv_scale, Imgproc.COLOR_RGB2HSV);
-
-        //find scalar sum of hsv
-        Scalar mColorHsv = Core.sumElems(hsv_scale);
-
-        int pointCount = 320*240;
-
-
-        //convert each pixel
-        for (int i = 0; i < mColorHsv.val.length; i++) {
-            mColorHsv.val[i] /= pointCount;
-        }
-
-        //convert hsv scalar to rgb scalar
-        Scalar mColorRgb = convertScalarHsv2Rgba(mColorHsv);
-
-        Core.inRange(mRgba,mColorHsv,mColorRgb,hsv_scale);
-
-        Size ksize =new Size(15,15);
-
-        Mat element; // = getStructuringElement(MORPH_RECT, Size(15, 15));
-        element = getStructuringElement(MORPH_RECT,ksize);
-        Imgproc.erode(mRgba,hsv_scale,element);
-        Imgproc.dilate(mRgba,hsv_scale,element);
-
-
-
-    /*Log.d("intensity", "Color: #" + String.format("%02X", (int)mColorHsv.val[0])
-            + String.format("%02X", (int)mColorHsv.val[1])
-            + String.format("%02X", (int)mColorHsv.val[2]) );*/
-        //print scalar value
-        Log.d("intensity", "R:"+ String.valueOf(mColorRgb.val[0])+" G:"+String.valueOf(mColorRgb.val[1])+" B:"+String.valueOf(mColorRgb.val[2]));
-
-
-        /*Convert to YUV*/
-
-        int R = (int) mColorRgb.val[0];
-        int G = (int) mColorRgb.val[1];
-        int B = (int) mColorRgb.val[2];
-
-        int Y = (int) (R *  .299000 + G *  .587000 + B *  .114000);
-        int U = (int) (R * -.168736 + G * -.331264 + B *  .500000 + 128);
-        int V = (int) (R *  .500000 + G * -.418688 + B * -.081312 + 128);
-
-        //int I = (R+G+B)/3;
-
-
-        //Log.d("intensity", "I: "+String.valueOf(I));
-        Log.d("intensity", "Y:"+ String.valueOf(Y)+" U:"+String.valueOf(U)+" V:"+String.valueOf(V));
-
-        /*calibration*/
-
-
-
-        return mRgba;
+        return imgThresholded;
     }
 
-    //convert Mat hsv to scalar
-    private Scalar convertScalarHsv2Rgba(Scalar hsvColor) {
-        Mat pointMatRgba = new Mat();
-        Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
-        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB);
-
-        return new Scalar(pointMatRgba.get(0, 0));
-    }
 
 
 }
